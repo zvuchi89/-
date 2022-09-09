1ad1adce3898b9fa8c53649b8162ad6e672f0ce3
Ekaterina <zvuchi89@mail.ru>
2022-09-09 16:29:10 +0300

    Zadacha13

---
+++ Zadacha13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число:");
+
+long n = Convert.ToInt64(Console.ReadLine());
+
+if (n > 99)
+{
+Console.Write("Третья цифра  "+ n.ToString()+" = "+ n.ToString()[2]);
+}
+if(n < 100 )
+ {
+    Console.WriteLine("третьей цифры нет");
+ }
